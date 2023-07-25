e26b90f112ca2b32fd3bfba9d7fe130954997586
Kester <ilokester@gmail.com>
2023-07-25 20:15:52 +0100

    first commit

---
+++ src/main/java/com/showsapp/showsapp/ShowsAppApplication.java
@@ -0,0 +1,13 @@
+package com.showsapp.showsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showsapp/showsapp/ShowsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showsapp.showsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
